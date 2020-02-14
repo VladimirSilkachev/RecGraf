656dd3f6e0bd365b445e2ce1055377013e5164b3
VladimirSilkachev <79515746577@yandex.ru>
2020-02-14 20:29:27 +0700

    Start

--- Fractals.py
+++ Fractals.py
@@ -1,6 +1,7 @@
 from turtle import *
-print('Выберите что хотите увидеть:\n 1-Снежинка по Коху\n 2-Ледяные фракталы\n 3-Дракон\n'
-      ' 4-Бинарное дервево\n 5- НЕ ЗАБЫТЬ ВСТАВИТЬ НАЗВАНИЕ\n(введите число)')
+
+print('Выберите что хотите увидеть:\n 1-Снежинка Коха\n 2-Фрактал Коха\n 3-Дракон\n'
+      ' 4-Бинарное дервево\n(введите число)')
 chose = input()
 
 
@@ -16,30 +17,41 @@ def snow(order, n):
         left(60)
         snow(order - 1, n / 3)
     for _ in range(3):
-        snow(4, 240)
+        snow(a, b)
         right(120)
+
 
 def ice():
     pass
 
+
 def dragon():
     pass
 
-def tree():
-    pass
 
-def kirill_func():
-    pass
-a = input('Введите глубину фрактала')
-b = input('Введите размер')
+def tree(a):
+    if a >= 1:
+        forward(a * 10)
+        right(30)
+        tree(a * 0.75)
+        left(60)
+        tree(a * 0.75)
+        right(30)
+        backward(a * 10)
+
+
+
+a = int(input('Введите глубину фрактала'))
+if chose != '4':
+    b = int(input('Введите размер'))
 if chose == 1:
-    snow(a,b)
+    snow(a, b)
 if chose == 2:
-    ice(a,b)
+    ice(a, b)
 if chose == 3:
-    dragon(a,b)
+    dragon(a, b)
 if chose == 4:
-    tree(a,b)
-if chose == 5:
-    tree(a,b)
+    left(90)
+    tree(a)
+
 done()
