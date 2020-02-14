ff6ea93c4bbeefed5c76e6c2e142fc837b68c341
VladimirSilkachev <79515746577@yandex.ru>
2020-02-14 21:45:43 +0700

    4/4 + kirill

--- Fractals.py
+++ Fractals.py
@@ -1,51 +1,65 @@
 from turtle import *
-
+# Project: fractals
+# Members: Vladimir Silkachev 40%, Vinnikov Alexandr 40%, Kirill Popov 25%.
 print('Выберите что хотите увидеть:\n 1-Снежинка Коха\n 2-Фрактал Коха\n 3-Дракон\n'
       ' 4-Бинарное дервево\n(введите число)')
-chose = input()
+chose = int(input())
 
 
-def snow(order, n):
-    if order == 0:
-        forward(n)
-    else:
-        snow(order - 1, n / 3)
-        left(60)
-        snow(order - 1, n / 3)
-        right(120)
-        snow(order - 1, n / 3)
-        left(60)
-        snow(order - 1, n / 3)
+def Koh_Snow(a,b):
+
+    def snow(a,b):
+        if a == 0:
+            forward(b)
+        else:
+            snow(a - 1, b / 3)
+            left(60)
+            snow(a - 1, b / 3)
+            right(120)
+            snow(a - 1, b / 3)
+            left(60)
+            snow(a - 1, b / 3)
     for _ in range(3):
-        snow(a, b)
+        snow(4,200)
         right(120)
 
 
-def ice():
-    pass
+def Koh(a, b):
+    if a == 0:
+        forward(b)
+    else:
+        Koh(a - 1, b / 3)
+        left(60)
+        Koh(a - 1, b / 3)
+        right(120)
+        Koh(a - 1, b / 3)
+        left(60)
+        Koh(a - 1, b / 3)
 
-long = 120
-num = 12
 
-# функция, которая возвращает набор поворотов для дракона, чтобы мы могли пройти в обратном напровлении
-def revers(lst):
-      otpt = []
-      for i in lst:
+def dragon(a,b):
+    speed(100)
+
+    # функция, которая возвращает набор поворотов для дракона, чтобы мы могли пройти в обратном напровлении
+    def revers(lst):
+        otpt = []
+        for i in lst:
             otpt.append(-1 * i)
-            otpt.reverse()
-            return otpt
+        otpt.reverse()
+        return otpt
 
-# функция, которая склеивает [набор повортов предыдущего дракона] + [поворто направо] + [набор поворотов в обратном направвлении]
-def dragon(n):
-            if n == 0:
+    # функция, которая склеивает [набор повортов предыдущего дракона] + [поворто направо] +
+    # [набор поворотов в обратном направвлении]
+    def drago(n):
+        if n == 0:
             return []
-      return dragon(n-1) + [1] + revers(dragon(n-1))
+        return drago(n - 1) + [1] + revers(drago(n - 1))
 
-# рисуем дракона по набору повортов
-t.forward(long / (num + 1))
-for i in dragon(num):
-      t.right(90*i)
-      t.forward(long / (num + 1))
+    # рисуем дракона по набору повортов
+    forward(b / (a + 1))
+    for i in drago(a):
+        right(90 * i)
+        forward(b / (a + 1))
 
 
 def tree(a):
@@ -59,19 +73,21 @@ def tree(a):
         backward(a * 10)
 
 
-
 a = int(input('Введите глубину фрактала'))
-if chose != '4':
+if chose != 4:
     b = int(input('Введите размер'))
+
 if chose == 1:
-    snow(a, b)
+    Koh_Snow(a,b)
+
 if chose == 2:
-    ice(a, b)
+    Koh(a, b)
+
 if chose == 3:
-    dragon(a, b)
+    dragon(a,b)
+
 if chose == 4:
     left(90)
     tree(a)
-
 
 done()
